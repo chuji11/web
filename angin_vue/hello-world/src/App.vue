d90b4aef0978fbb7037f23e80e1cf2978d59addb
zhujiawen <249707696@qq.com>
2021-06-03 20:31:37 +0800

    0603

--- angin_vue/hello-world/src/App.vue
+++ angin_vue/hello-world/src/App.vue
@@ -1,7 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <Tabs type="card" @on-contextmenu="handleContextMenu">
+           <TabPane label="标签一" name="tab1" context-menu>标签一的内容</TabPane>
+           <TabPane label="标签二" name="tab2">标签二的内容</TabPane>
+           <TabPane label="标签三" name="tab3" context-menu>标签三的内容</TabPane>
+           <template slot="contextMenu">
+               <DropdownItem @click.native="handleContextMenuEdit">编辑</DropdownItem>
+               <DropdownItem @click.native="handleContextMenuDelete" style="color: #ed4014">删除</DropdownItem>
+           </template>
+       </Tabs>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+
+    <input type="text" @input="change" name="" id="" :value="num">
   </div>
 </template>
 
@@ -11,7 +22,38 @@ import HelloWorld from './components/HelloWorld.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+  },
+  data:()=>{
+    return {
+      // num: 0,
+    }
+  },
+  computed:{
+    num:{
+
+      get: function (value) {
+        return value
+      },
+      // setter
+      set: function (newValue) {
+        var names = newValue;
+        console.log(names)
+        return newValue
+      }
+    }
+
+  },
+  methods:{
+    handleContextMenu(){
+
+    },
+    handleContextMenuEdit(){
+      
+    },
+    change(e){
+      console.log(e)
+    }
   }
 }
 </script>
